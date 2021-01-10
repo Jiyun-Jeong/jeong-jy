954173f6288a8cce6fa22254c3f610f131d19e41
html2020-2 <58695492+html2020-2@daum.net>
2021-01-10 23:46:43 +0900

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.bigMainCircle').addClass('up');
   setTimeout(function () {
-    $('.bigMainCircle').removeClass('up').css({overflow: 'visible', top: '50%'}).stop().animate({left: '-200%'}, 1000);
-    $('.bigMainCircle img').css({transition: 'none', left: '-20%'}).stop().animate({left: '230%'}, 1000);
+    $('.bigMainCircle').removeClass('up').css({overflow: 'visible', top: '50%'}).stop().animate({left: '-200%'}, 900);
+    $('.bigMainCircle img').css({transition: 'none', left: '-20%'}).stop().animate({left: '230%'}, 900);
   }, 1500);
 });
