4b5f0bc6def0ab9ff7f1fd85fd371d837fbb176f
html2020-2 <58695492+html2020-2@daum.net>
2021-01-10 23:44:14 +0900

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.bigMainCircle').addClass('up');
   setTimeout(function () {
-    $('.bigMainCircle').removeClass('up').css({overflow: 'visible', top: '50%'}).stop().animate({left: '-200%'}, 2000);
-    $('.bigMainCircle img').css({transition: 'none', left: '-20%'}).stop().animate({left: '230%'}, 2000);
+    $('.bigMainCircle').removeClass('up').css({overflow: 'visible', top: '50%'}).stop().animate({left: '-200%'}, 1000);
+    $('.bigMainCircle img').css({transition: 'none', left: '-20%'}).stop().animate({left: '230%'}, 1000);
   }, 1500);
 });
