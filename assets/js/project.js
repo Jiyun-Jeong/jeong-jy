9e20c3a01fea402594adae3b37b62a96363d9a25
html2020-2 <72422095+html2020-2@daum.net>
2021-01-11 10:44:03 +0900

    sub gopro 상단 animate

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $('.bigMainCircle').addClass('up');
   setTimeout(function () {
-    $('.bigMainCircle').removeClass('up').css({overflow: 'visible', top: '50%'}).stop().animate({left: '-200%'}, 900);
-    $('.bigMainCircle img').css({transition: 'none', left: '-20%'}).stop().animate({left: '230%'}, 900);
+    $('.bigMainCircle').removeClass('up').addClass('leftout');
   }, 1500);
 });
